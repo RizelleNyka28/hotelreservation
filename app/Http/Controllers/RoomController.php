6e44cb05e4e45594f036e873881f47223c22dc8e
RizelleNyka28 <122860654+RizelleNyka28@users.noreply.github.com>
2023-02-28 06:20:19 +0800

    up

---
+++ app/Http/Controllers/RoomController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RoomController extends Controller
+{
+    
+    public function index()
+    {
+        return 'Rooms';
+    }
+}
